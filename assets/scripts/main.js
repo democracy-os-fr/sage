24c3cfa53341d3aa7bfce5339e6316b0013b34e2
moustachu <git@moustachu.net>
2016-09-27 03:37:35 +0200

    Revert "merging with Bootstrap starterkit"
    This reverts commit fe95d506f41989a1c07f6fdfd57c4d2e20004dae.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,6 @@
 // import external dependencies
 import 'jquery';
-import 'bootstrap-sass/assets/javascripts/bootstrap';
+import 'bootstrap/dist/js/bootstrap';
 
 // import local dependencies
 import Router from './util/router';
