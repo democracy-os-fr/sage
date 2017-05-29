2febc3821b504113ab58e5682c800cd79b332b39
moustachu <git@moustachu.net>
2017-05-29 20:03:20 +0200

    caraousel

--- assets/scripts/routes/Common.js
+++ assets/scripts/routes/Common.js
@@ -2,6 +2,13 @@ export default {
   init() {
     // JavaScript to be fired on all pages
     // console.log('JS sage :: routes/common :: init');
+    $('.carousel').carousel({
+      interval: 5000,
+    })
+    .on('slide.bs.carousel', (e) => {
+      const nextH = $(e.relatedTarget).outerHeight();
+      $(this).find('.active.item').parent().animate({ height: nextH }, 700);
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
