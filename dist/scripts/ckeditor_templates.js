9c9dac567c17d3941edcc6e4f03433585a7d55a4
moustachu <git@moustachu.net>
2018-11-19 22:50:56 +0100

    New typology

--- dist/scripts/ckeditor_templates.js
+++ dist/scripts/ckeditor_templates.js
@@ -6,9 +6,9 @@
 /******/ 	function __webpack_require__(moduleId) {
 /******/
 /******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
+/******/ 		if(installedModules[moduleId]) {
 /******/ 			return installedModules[moduleId].exports;
-/******/
+/******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			i: moduleId,
@@ -63,12 +63,12 @@
 /******/ 	__webpack_require__.p = "/themes/contrib/sage/dist/";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 17);
+/******/ 	return __webpack_require__(__webpack_require__.s = 12);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 17:
+/***/ 12:
 /***/ (function(module, exports) {
 
 
